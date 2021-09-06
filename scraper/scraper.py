ea7db974748046b2b6a81c63d8c7bc0096cb3685
nfox-ai <lythris@protonmail.ch>
2021-09-06 23:07:46 +0200

    Remove trailing commas in csv-file write

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -24,8 +24,12 @@ with open('keywords.txt', 'r', encoding = 'UTF-8') as f:
 
 # Overwrite previous links file & get the handle
 links_file = open("links.csv", "w")
+# Write column header
+links_file.write('"URL"\n')
 # Overwrite previous result : keyword file & get the handle
 results_file = open("results.csv", "w")
+# Write column header
+results_file.write('"Results", "Keyword"\n')
 
 # Parse the page looking for URLs pointing at our target & 
 # write finds to the links csv file
@@ -35,7 +39,7 @@ def getSearchPageURL(soup):
     for link in soup.find_all('a', href = True):
         url = link['href']
         if url[:36] == 'https://www.searchenginejournal.com/':
-            links_file.write(url + ',\n')
+            links_file.write(url + '\n')
 
 # Parse the page looking for 'result-stats' div to obtain total amount of
 # indexed pages related to the search & save to file
@@ -46,7 +50,7 @@ def getResultsCount(soup, keyword):
         resultCount = 0
     else:
         resultCount = resultDiv.text.split(" ")[1]
-    results_file.write(str(resultCount) + ", " + keyword + ",\n")
+    results_file.write(str(resultCount) + ", " + keyword + "\n")
 
 
 # Crawl search pages for given keyword
